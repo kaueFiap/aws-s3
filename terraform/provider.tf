4ff2eedf4598a413d63b2b4a73f2b9e3ca6c4aad
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-13 21:57:25 -0300

    Configuração inicial do Terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,10 +8,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 6.8.0"
     }
-  }
+  }   
 
   backend "s3" {
-    bucket       = "aws-s3-tfstate-kledsonbasso"
+    bucket       = "my-bucket-kaaue-backend"
     key          = "tfstate"
     region       = "us-east-1"
     use_lockfile = true
